8032f0742af9b75d4efc7559eea4572ca297d191
josestrk <josenass22@gmail.com>
2014-10-14 14:13:56 +0200

    Arreglado pattern

--- js/validate.js
+++ js/validate.js
@@ -1,20 +1,20 @@
-
 function validatePattern(cadena,patron){
-    return (cadena.match(patron));
+  return cadena.match(patron) !== null;
 }
 
 function validateUser(cadena) {
-    return (validatePattern(cadena,'/[a-zA-Z0-9áéíóúñüÁÉÍÓÚñÜ_\-]/') && cadena.length >= 4 && cadena.length <= 30) ;
+    return (validatePattern(cadena,/^[a-z0-9áéíóúñ_/-]*$/i) && cadena.length >= 4 && cadena.length <= 30) ;
 }
 function validateEmail(cadena) {
-    return validatePattern(cadena,'/[\w-\.]{3,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/');
+    return validatePattern(cadena,/[\w-\.]{3,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/);
 }
 function validatePass(cadena) {
-    return (validatePattern(cadena,'(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,16})$'));
+    return validatePattern(cadena,/(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,16})$/);
 }
 
 module.exports = {
 	validateUser : validateUser(),
 	validatePass : validatePass(),
 	validateEmail : validateEmail()
-}
+ };
+ 
