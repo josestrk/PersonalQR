a4dc1cf82c260a706cc28b01ecb1311161c1eac7
Alevale <alexvales111@gmail.com>
2014-10-23 18:17:42 +0200

    Server working with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,21 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		jshint: {
 			src: ['Gruntfile.js', 'features/**/*.js', 'model/**.js']
+		},
+		connect: {
+		    server: {
+		      options: {
+			port: 9001,
+			base: 'www-root'
+		      }
+		    }
 		}
 	});
 	
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	
-	grunt.registerTask('default', ['jshint']);
+
+	grunt.loadNpmTasks('grunt-connect');
+
+	grunt.registerTask('default', ['jshint', 'connect']);
 
 };
