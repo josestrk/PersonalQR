918930a36829941cb38dfabd32a690ba82685a3d
dgstranz <dgstranz@gmail.com>
2014-11-21 01:33:28 +0100

    topic routes & a bit of AJAX

--- server/manager/manager_topic.js
+++ server/manager/manager_topic.js
@@ -2,33 +2,39 @@ var daoTopic = require('../dao/dao_topic');
 
 //importas del objeto export{} de topic.js el metodo de creacion de usuarios, y sus metodos asociados
 function create(callback) {
-    var temporalTopic = {
-        name:  ""
-    };
-    daoTopic.create(temporalTopic, callback);
+  var temporalTopic = {
+    title : "",
+    content : "",
+    tags : "",
+    topics : "",
+    likes: "",
+    comments: new Array(),
+    date : ""
+  };
+  daoTopic.create(temporalTopic, callback);
 }
 
 function get(topicId, callback) {
-    daoTopic.getById(topicId, callback);
+  daoTopic.getById(topicId, callback);
 }
 
 function getAll(callback) {
-    daoTopic.getAll(callback);
+  daoTopic.getAll(callback);
 }
 
 function setTopic(topicId, data, callback){
-    daoTopic.setTopic(topicId, data, callback);
+  daoTopic.setTopic(topicId, data, callback);
 }
 
 function del(topicId, callback) {
-    daoTopic.delTopic(topicId, callback);
+  daoTopic.del(topicId, callback);
 }
 
 module.exports = {
-	createTopic: create,
-	getTopic: get,
-	getAllTopics: getAll,
-	setTopic: setTopic,
+  createTopic: create,
+  getTopic: get,
+  getAllTopics: getAll,
+  setTopic: setTopic,
   delTopic: del
 };
 
