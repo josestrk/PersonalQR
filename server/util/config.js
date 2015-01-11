c0815cb21195de1d9c65729ecf353c23c6d3d9e9
alevale <alexvales111@gmail.com>
2015-01-11 05:16:50 +0100

    Cambios de estilo y implementado el scroll en las paginas, y en los articulos, dejado plantado el sistema de comentarios a falta de editar un poco los temas del estilo y demas.

--- server/util/config.js
+++ server/util/config.js
@@ -9,13 +9,23 @@ config = {
 		}
 	};
 
-module.exports = config;
+	// 	//para servir desde local con linux, sino no deja...
+	// 	server: {
+	// 		ip: process.env.IP || '0.0.0.0',
+	// 		port: process.env.PORT || 9005
+	// 	},
+	// 	db: {
+	// 		conn: 'mongodb://0.0.0.0:27017/pqr_db'
+	// 	}
+	// };
 
-//revisar cuando tengamos openshift desplegado
-// if (process.env.CONFIG) {
-// 	config = require('../config/' + process.env.CONFIG);
-// } else {
-//   // config = require('../config/dev');
+	module.exports = config;
+
+	//revisar cuando tengamos openshift desplegado
+	// if (process.env.CONFIG) {
+	// 	config = require('../config/' + process.env.CONFIG);
+	// } else {
+	//   // config = require('../config/dev');
 // 	config = {
 // 		server: {
 // 			ip: process.env.IP || '0.0.0.0',
