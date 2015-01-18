e64daf8f7d877e77e70bb4779c4cd017120940d3
josestrk <josenass22@gmail.com>
2015-01-18 19:41:00 +0100

    Add menu comment and functtion button coment

--- TODO.md
+++ TODO.md
@@ -1,4 +1,4 @@
 Conecttion to Mongo in Windows
 
 cd 'C:\Program Files\MongoDB 2.6 Standard\bin\'
-.\mongo.exe
+.\mongod.exe
--- server/util/config.js
+++ server/util/config.js
@@ -1,23 +1,23 @@
 var config;
 config = {
-	// 	server: {
-	// 		ip: process.env.IP || '127.0.0.1',
-	// 		port: process.env.PORT || 9005
-	// 	},
-	// 	db: {
-	// 		conn: 'mongodb://127.0.0.1:27017/pqr_db'
-	// 	}
-	// };
-
-		//para servir desde local con linux, sino no deja...
 		server: {
-			ip: process.env.IP || '0.0.0.0',
+			ip: process.env.IP || '127.0.0.1',
 			port: process.env.PORT || 9005
 		},
 		db: {
-			conn: 'mongodb://0.0.0.0:27017/pqr_db'
+			conn: 'mongodb://127.0.0.1:27017/pqr_db'
 		}
 	};
+
+	// 	//para servir desde local con linux, sino no deja...
+	// 	server: {
+	// 		ip: process.env.IP || '0.0.0.0',
+	// 		port: process.env.PORT || 9005
+	// 	},
+	// 	db: {
+	// 		conn: 'mongodb://0.0.0.0:27017/pqr_db'
+	// 	}
+	// };
 
 	module.exports = config;
 
