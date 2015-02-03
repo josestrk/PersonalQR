d8d23959a181099c5e786a51273698f7003c0be8
josetrk <josenass22@gmail.com>
2015-02-03 14:19:21 +0100

    grunt start

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,165 +1,126 @@
 var conf_port=9001;
 module.exports = function(grunt) {
 
-      grunt.initConfig({
-//		Import package manifest
-      pkg: grunt.file.readJSON("package.json"),
+grunt.initConfig({
+  //Import package manifest
+  pkg: grunt.file.readJSON("package.json"),
 
-      connect: {
-		    server: {
-			        port: 9001
-		    }
-		  },
-
-      shell: {
-			runLocalServer: {
-				command: 'DEBUG=personalqr* node server/server'
-			},
-			runLocalServerWin32: {
-				command: [
-					'set DEBUG=personalqr*',
-					'node server/server'
-				].join('&&'),
-                command: [
-					'cd C:/Program Files/MongoDB 2.6 Standard/bin/',
-                    './mongod.exe '
-				].join('&&')
-			}
-//			,
-//          dredd: {
-//				command: 'CONFIG=dredd dredd ./server/api/apiary/apiary.apib http://localhost:'+conf_port;
-//			}
-		},
-
-        jshint: {
-			server: {
-				src: ['Gruntfile.js',
-                        'server/dao/**/*.js',
-                        'server/manager/**/*.js',
-                        'server/middleware/**/*.js',
-                        'server/routes/**/*.js',
-                        'server/util/**/*.js',
-                        'server/server.js'
-						]
-			},//server
-			client: {
-				options: {
-					extract: 'auto'
-				},
-				src: ['*.html', 'pages/**/*.html', 'core/**/*']
-			}//client
-		},//jshint
-        vulcanize: {
-			default: {
-				options: {
-					csp: true,
-					excludes: {
-						imports: [
-							"bower_compoents/polymer.html"
-						]
-					}
-				},
-				files: {
-					'_/index.html': 'index.html'
-				},
-	         }
-		},//vulcanize
-        uglify: {
-          my_target: {
-            files: {
-            '_/js/middleware.min.js':['/server/middleware/*'],
-            '_/js/util.min.js':['/server/util/*'],
-            '_/js/server.min.js':['/server/server.js']
-            }
-          } //my_target
-        },//uglify
-        imagemin: {
-
-            main: {
-
-              files: [{
-                expand: true,
-                cwd: '/img/',
-                src: ['**/*.{png,jpg,gif,.svg}'],
-                dest: '_/img/'
-              }]
-
-            }
-        },//imagemin
-        browserSync: {
-
-          dev: {
-            bsFiles: {
-              src : [
-                '/core/*',
-                '/core/**/*.html',
-                '/core/**/*.js',
-                '/core/**/*.css',
-                '/img/**/*.jpg',
-                '/img/**/*.png',
-                '/img/**/*.svg',
-                '/pages/**/*.html',
-                '**/*.html'
-                ]
-              },
-            options: {
-                watchTask: true,
-                debugInfo: true,
-                logConnections: true,
-                notify: true,
-                proxy: "personalqr.ds:8000",
-                ghostMode: {
-                  scroll: true,
-                  links: true,
-                  forms: true
-                }
-            }
-          } //dev
-        }, // browserSync
-        watch: {
-
-          scripts: {
-            files: ['**/*.js'],
-            tasks: ['uglify'],
-            options: {
-              spawn: false,
-            }
-          } //scripts
-//        , images: {
-//
-//            files: ['_/img/*.{png,jpg,gif}'],
-//            tasks: ['imagemin'],
-//            options: {
-//              spawn: false,
-//            }
-//          }//images
-        } //watch
-
-      });
+  connect: {
+    server: {
+	        port: 9001
+    }
+  },//connect
+  shell: {
+  	runLocalServer: {
+  		command: 'DEBUG=pqr* node server/server'
+  	},
+  	runLocalServerWin32: {
+  		command: [
+  			'set DEBUG=personalqr*',
+  			'node server/server'
+  		].join('&&'),
+              command: [
+  			'cd C:/Program Files/MongoDB 2.6 Standard/bin/',
+        '.\\mongod.exe '
+  		].join('&&')
+  	}
+  },//shell
+  jshint: {
+  	server: {
+  		src: ['Gruntfile.js',
+                      'server/dao/**/*.js',
+                      'server/manager/**/*.js',
+                      'server/middleware/**/*.js',
+                      'server/routes/**/*.js',
+                      'server/util/**/*.js',
+                      'server/server.js'
+  				]
+  	},//server
+  	client: {
+  		options: {
+  			extract: 'auto'
+  		},
+  		src: ['pages/**/*.html', 'core/**/*.html']
+  	}//client
+  },//jshint
+  vulcanize: {
+  	default: {
+  		options: {
+  			csp: true,
+  			excludes: {
+  				imports: ["bower_compoents/polymer.html"]
+  			}
+  		},
+  		files: {
+  			'_/index.html': 'index.html'
+  		},
+    }
+  },//vulcanize
+  browserSync: {
+    dev: {
+      bsFiles: {
+        src : [
+          '/core/*',
+          '/core/**/*.html',
+          '/core/**/*.js',
+          '/core/**/*.css',
+          '/img/**/*.jpg',
+          '/img/**/*.png',
+          '/img/**/*.svg',
+          '/pages/**/*.html',
+          '**/*.html'
+          ]
+        },
+      options: {
+          watchTask: true,
+          debugInfo: true,
+          logConnections: true,
+          notify: true,
+          proxy: "personalqr.ds:8000",
+          ghostMode: {
+            scroll: true,
+            links: true,
+            forms: true
+          }
+      }
+    } //dev
+  } // browserSync
+});
 
 
-    grunt.loadNpmTasks('grunt-contrib-watch'); //control modificates in live
-    grunt.loadNpmTasks('grunt-contrib-uglify');//minificar js
-    grunt.loadNpmTasks('grunt-contrib-imagemin');//minificar image
-    grunt.loadNpmTasks('grunt-contrib-cssmin');  //minificar css
-    grunt.loadNpmTasks('grunt-contrib-htmlmin'); //minifica html
-	grunt.loadNpmTasks('grunt-contrib-jshint'); //tester
-    grunt.loadNpmTasks('grunt-newer');// check changes
-    grunt.loadNpmTasks('grunt-browser-sync'); // nav-syncronizated
-	grunt.loadNpmTasks('grunt-connect');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-connect');
+  grunt.loadNpmTasks('grunt-vulcanize'); // enpaquetador
+  grunt.loadNpmTasks('grunt-contrib-jshint'); //ortografi check
+  grunt.loadNpmTasks('grunt-browser-sync'); // nav-syncronizated
 
-    grunt.log.writeln('Start PersonalQR:');
+	// grunt.registerMultiTask('dist-client', 'Prepare client files to distribution', function() {
+	// 	var inDir = '...';
+	// 	var outDir = inDir + 'dist/';
+	// 	grunt.log.writeln('Removing client/dist directory...');
+	// 	var done = this.async();
+	// 	fs.remove(outDir, function(err, result) {
+	// 		if (err) {
+	// 			grunt.log.error(err);
+	// 			done(err);
+	// 		} else {
+	// 			fs.mkdirsSync(outDir);
+	// 			grunt.task.run('vulcanize');
+	// 			fs.copySync(inDir + 'font', outDir + 'font');
+	// 			fs.copySync(inDir + 'img', outDir + 'img');
+	// 			fs.copySync(inDir + 'bower_components', outDir + 'bower_components');
+	// 			fs.copySync(inDir + 'css', outDir + 'css');
+	// 			fs.copySync(inDir + 'elements/point-setter/presenter.js', outDir  + 'elements/point-setter/presenter.js'); //TODO Minify this- Vulvanize can't do it
+	// 			done();
+	// 		}
+	// 	});
+	// });
 
-    //TYPES RUN
-    grunt.registerTask('connect',['connect']);
-	  grunt.registerTask('default',['jshint:client', 'jshint:server', 'shell: runLocalServer','watch']);
-    grunt.registerTask('win',['jshint:client', 'jshint:server', 'shell:runLocalServerWin32','watch']);
-    grunt.registerTask('run', function() {
-		(process.platform === "win32") ? grunt.task.run('shell: runLocalServerWin32') : grunt.task.run('shell: runLocalServer')
-	});
-	grunt.registerTask('sync', ['browserSync' , 'watch']);
-    //symplificator
-    grunt.registerTask('minifi', ['imagenmin', 'uglify' , 'cssmin', 'htmlmin', 'newer' , 'watch']);
+  grunt.log.writeln('Start PersonalQR:');
 
-    grunt.log.writeln('Readdy... http://localhost:9001');
+  //TYPES RUN
+  grunt.registerTask('default', function() {(process.platform === "win32") ? grunt.task.run('shell:runLocalServerWin32') : grunt.task.run('shell:runLocalServer')});
+  grunt.registerTask('check',['jshint:client', 'jshint:server', 'shell:runLocalServer']);
+  grunt.registerTask('win',['jshint:client', 'jshint:server', 'shell:runLocalServerWin32']);
+	grunt.registerTask('sync', ['browserSync']);
 };
